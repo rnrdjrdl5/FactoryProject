5eb4bd503fe1b2cd04c2d6fd7eae543a495444ea
jaeeokban <jaeeok.ban@devsisters.com>
2026-04-10 15:59:10 +0900

    플레이어 Animator Dispatcher 추가

---
+++ Assets/Contents/Player/Script/PlayerAnimationMsg.cs
@@ -0,0 +1,17 @@
+public static partial class PlayerAnimationMsg
+{
+    public struct AttackFireMsg
+    {
+        public Player Player;
+    }
+
+    public struct ComboOpenMsg
+    {
+        public Player Player;
+    }
+
+    public struct ComboCloseMsg
+    {
+        public Player Player;
+    }
+}
