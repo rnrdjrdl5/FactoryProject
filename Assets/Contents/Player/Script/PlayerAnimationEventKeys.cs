b6c97b848f5654f1a0e6f5580eee103bbc5d400b
jaeeokban <jaeeok.ban@devsisters.com>
2026-04-10 15:59:10 +0900

    플레이어 Animator Dispatcher 추가

---
+++ Assets/Contents/Player/Script/PlayerAnimationEventKeys.cs
@@ -0,0 +1,6 @@
+public static class PlayerAnimationEventKeys
+{
+    public const string AttackFire = "AttackFire";
+    public const string ComboOpen = "ComboOpen";
+    public const string ComboClose = "ComboClose";
+}
