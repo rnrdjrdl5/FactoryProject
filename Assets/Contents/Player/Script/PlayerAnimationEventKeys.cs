918768cc53db793d559610060b0fef88a8d6df36
infy_mac <infy_mac@infy-macui-Macmini.local>
2026-04-10 18:20:43 +0530

    Revert "플레이어 Animator Dispatcher 추가"
    This reverts commit 5eb4bd503fe1b2cd04c2d6fd7eae543a495444ea.

--- Assets/Contents/Player/Script/PlayerAnimationEventKeys.cs
+++
@@ -1,6 +0,0 @@
-public static class PlayerAnimationEventKeys
-{
-    public const string AttackFire = "AttackFire";
-    public const string ComboOpen = "ComboOpen";
-    public const string ComboClose = "ComboClose";
-}
